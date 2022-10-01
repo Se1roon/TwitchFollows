aee6988b73f8c686b8c7c27fbc05e426b0d45b78
Se1roon <se1roonm@gmail.com>
2022-10-01 11:12:25 +0200

    Little changes in Button.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
 
-const Button = ({ text, url, link = true, useLink = true }) => {
+const Button = ({
+  text,
+  url,
+  type,
+  button = false,
+  link = true,
+  useLink = true,
+  onClick,
+}) => {
   if (!link) useLink = false;
+
+  const renderButton = () => {
+    return (
+      <button className="btn" type={type} onClick={onClick}>
+        {text}
+      </button>
+    );
+  };
 
   const renderLinkWithLink = () => {
     return (
@@ -24,7 +40,7 @@ const Button = ({ text, url, link = true, useLink = true }) => {
   };
 
   const getRenderFunc = () => {
-    console.log(link, useLink, text);
+    if (button) return renderButton();
     if (link && useLink) return renderLinkWithLink();
     if (link && !useLink) return renderLinkWithoutLink();
     if (!link) return renderWithoutLink();
