50e9ded4e550ef291d05c178c3edb0213e443fc4
Se1roon <se1roonm@gmail.com>
2022-10-02 09:18:54 +0200

    Add getUsersWithId.

---
+++ src/utils/getUserWithId.js
@@ -0,0 +1,21 @@
+import createHeaders from "./createHeaders";
+
+export default async function getUserWithId(
+  userId,
+  authorizationObj,
+  clientId
+) {
+  const endpoint = `https://api.twitch.tv/helix/users?id=${userId}`;
+
+  let { access_token, token_type } = authorizationObj;
+
+  token_type =
+    token_type.substring(0, 1).toUpperCase() +
+    token_type.substring(1, token_type.length);
+
+  const headers = createHeaders(token_type, access_token, clientId);
+
+  return fetch(endpoint, { headers })
+    .then((res) => res.json())
+    .then((data) => data);
+}
